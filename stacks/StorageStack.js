e241c489fd616d947221dc15b26a1f1e6efb30e9
Patrick-Vincent Herrera <59979695+pvherreramusic@users.noreply.github.com>
2022-03-19 18:46:51 -0700

    Adding a storage stack

---
+++ stacks/StorageStack.js
@@ -0,0 +1,23 @@
+import * as sst from "@serverless-stack/resources";
+
+export default class StorageStack extends sst.Stack {
+  // Public reference to the table
+  // Public reference to the bucket
+bucket;
+  table;
+
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    // Create the DynamoDB table
+    // Create an S3 bucket
+this.bucket = new sst.Bucket(this, "Uploads");
+    this.table = new sst.Table(this, "Notes", {
+      fields: {
+        userId: sst.TableFieldType.STRING,
+        noteId: sst.TableFieldType.STRING,
+      },
+      primaryIndex: { partitionKey: "userId", sortKey: "noteId" },
+    });
+  }
+}
